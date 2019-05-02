1576b0ef88298266223e11bb5ec3ca98b6568071
Dheeraj Karki <dheerajkarki1790@outlook.com>
2019-05-02 20:33:11 +0100

    Correcting premake5.lua and adding .gitignore.

--- premake5.lua
+++ premake5.lua
@@ -14,10 +14,16 @@ project "Glad"
 
     includedirs "./include"
 
-    buildoptions { "-std=c11", "-lgdi32" }
-    systemversion "latest"
-    staticruntime "On"
+
     filter "system:windows"
+        buildoptions { "-std=c11", "-lgdi32" }
+        systemversion "latest"
+        staticruntime "On"
 
     filter { "system:windows", "configurations:Release" }
         buildoptions "/MT"
+
+    filter "system:macosx"
+        buildoptions { "-std=c11" }
+        systemversion "latest"
+        staticruntime "On"
