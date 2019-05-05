1f730e5d84f66cc9f74612e6e7c0f17a006113b1
Dheeraj Karki <dheerajkarki1790@outlook.com>
2019-05-05 21:31:55 +0100

    Refactor and correct indentation of premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,29 +1,26 @@
 project "Glad"
     kind "StaticLib"
     language "C"
+    systemversion "latest"
+    staticruntime "on"
+    buildoptions { "-std=c11" }
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
     files
     {
-    "include/KHR/khrplatform.h",
-    "include/glad/glad.h",
-    "src/glad.c"
+        "include/KHR/khrplatform.h",
+        "include/glad/glad.h",
+        "src/glad.c"
     }
 
     includedirs "./include"
 
-
+    --Visual Studio IDE specific setting for Window system
     filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
-        systemversion "latest"
-        staticruntime "On"
-
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
-
-    filter "system:macosx"
-        buildoptions { "-std=c11" }
-        systemversion "latest"
-        staticruntime "On"
+        buildoptions { "-lgdi32" }
+    
+    --Configuration specific settings for all system
+    filter "configurations:Release"
+        runtime "Release"
